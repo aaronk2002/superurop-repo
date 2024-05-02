7181b9fd756436f40c6eb68bc593fab749bd2e45
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2024-05-02 18:26:42 -0400

    Added bash_scripts back

---
+++ bash_scripts/run-correlations-W.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Loading Modules in Supercloud
+source /etc/profile
+module load anaconda/2023b
+module load cuda/11.8
+
+# Variables
+parameterization=W
+att_svm_p=( 2 1_75 1_75 3 3 2 1_75 2 3 ) 
+Ws_p=( 3 3 2 2 1_75 1_75 1_75 2 3 ) 
+
+# For Loop
+for seed in {1..100}
+do
+    for idx in "${!att_svm_p[@]}"
+    do
+        i=${att_svm_p[$idx]}
+        j=${Ws_p[$idx]}
+        python ../correlation_calculation.py --att_svm ../result/convergence/p$i/$parameterization$seed --Ws ../result/convergence/p$j/$parameterization$seed --output ../result/correlation/$i-$j/$parameterization$seed
+    done
+done
---
+++ bash_scripts/run-local-1_75.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Loading Modules in Supercloud
+source /etc/profile
+module load anaconda/2023b
+module load cuda/11.8
+
+# Aggregated Result
+for seed in {1..100}
+do
+   python ../local_convergence.py -n 6 -T 8 -d 10 -p 1.75 --lr 0.1 --epochs 1_500 --seed $seed --normalized --parameterization W --std 1 --output ../result/convergence/p1_75/W$seed
+   python ../local_convergence.py -n 6 -T 8 -d 10 -p 1.75 --lr 0.1 --epochs 2_000 --seed $seed --normalized --parameterization KQ --std 1 --output ../result/convergence/p1_75/KQ$seed
+done
+
+# Single Result
+python ../local_convergence.py -n 6 -T 8 -d 10 -p 1.75 --lr 0.1 --epochs 1_500 --seed 1 --normalized --parameterization W --std 0 --output ../result/convergence/p1_75/SingleW
+python ../local_convergence.py -n 6 -T 8 -d 10 -p 1.75 --lr 0.1 --epochs 2_000 --seed 1 --normalized --parameterization KQ --std 0.01 --output ../result/convergence/p1_75/SingleKQ
